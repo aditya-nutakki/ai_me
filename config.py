d006b011cb5f461512b7ac6f6b33bfe39489d5de
Aditya Nutakki <adityanutakki6250@gmail.com>
2023-07-01 21:00:33 +0530

    added combining script for datasets

---
+++ config.py
@@ -0,0 +1,2 @@
+window_size = 16
+text_file_name = "./datasets/combined.txt" # file to train model from
---
+++ prepare_dataset.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import config as c
+
+
+def write_file(save_dir, text):
+    with open(save_dir, "w") as f:
+        for line in text:
+            f.write(line)
+
+
+def main():
+    dataset_path = os.path.dirname(c.text_file_name)
+    os.makedirs(dataset_path, exist_ok=True)
+    conversations_path = sys.argv[1] # D:\work\datasets\circles\processed
+
+    converstations = os.listdir(conversations_path)
+
+    combined = []
+    for conversation in converstations:
+        if conversation.endswith(".txt"):
+            converstation_path = os.path.join(conversations_path, conversation)
+            with open(converstation_path, "r") as f:
+                conversation = f.readlines()
+                combined.extend(conversation)
+
+    write_file(c.text_file_name, combined)
+    print(f"saved combined text dataset to {c.text_file_name} !")
+
+
+if __name__ == "__main__":
+    main()
